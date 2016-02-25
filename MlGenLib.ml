c6107e6000515ce9e395993439b602d8c91ec786
DariaDicu <daria.dicu@gmail.com>
2016-02-25 22:31:23 +0000

    Added parse functions in core library and generator. Does not work yet.

--- MlGenLib.ml
+++ MlGenLib.ml
@@ -1,8 +1,20 @@
+(*Things to do:
+- generate init function for ML structures
+- use init function to create empty object when first calling parseNextField
+- write a generic function for parsing messages
+- write a function for toplevel messages to strip the key
+*)
+
+
+
 (* For now, this is just a Word8Vector, it does not have a channel underneath*)
 (* Buffer is a vector and an index in the vector*)
 (* For now only considering integers, no floats or zigzags *)
 type byte = Word8.word
 type key = int * int
+datatype tag = Tag of int
+datatype code = Code of int
+datatype parsedByteCount = ParsedByteCount of int
 datatype errorCode = PARSE | ENCODE;
 exception Exception of errorCode*string
 
@@ -24,21 +36,19 @@ end
 
 signature BYTE_BUFFER = 
 sig
-	type byte_vector
 	type buffer
-	val fromVector : byte_vector -> buffer
+	val fromVector : Word8Vector.vector -> buffer
 	val fromList  : byte list -> buffer
 	(* Modifies buffer by putting index after next byte *)
 	val nextByte : buffer -> byte * buffer
 	(* Modifies buffer by putting index after next block. Returns both 
 	buffers, first the next block, then the remaining one. *)
-	val nextFixedBlock : buffer -> int -> buffer * buffer
+	val nextFixedBlock : buffer -> int -> Word8Vector.vector * buffer
 end
 
 structure ByteBuffer :> BYTE_BUFFER =
 struct
-	type byte_vector = Word8Vector.vector
-	type buffer = byte_vector * int
+	type buffer = Word8Vector.vector * int
 	fun fromVector buff = (buff, 0)
 	fun fromList l = fromVector (Word8Vector.fromList l)
 	fun nextByte (buff, i) =
@@ -56,57 +66,107 @@ struct
 			val block = Word8Vector.tabulate (length, fn n =>
 							Word8Vector.sub (buff, i+n))
 		in
-			((block, 0), (buff, i+length))
+			(block, (buff, i+length))
 		end
 end
 
-fun parseVarint_core buff i prev_val = 
+datatype parseResult = ParseResult of ByteBuffer.buffer*parsedByteCount
+
+fun parseVarint_core buff i prev_val (ParsedByteCount(s)) = 
 let 
 	val (b, next_buff) = ByteBuffer.nextByte buff
 	(* TODO - Treat overflow? *)
 	(* little endian *)
 	val next_val = 
-		prev_val + IntInf.<<((MlGenByte.toInt (MlGenByte.getTail b)), Word.fromInt(i * 7))
+		prev_val + IntInf.<<((MlGenByte.toInt (MlGenByte.getTail b)), 
+			Word.fromInt(i * 7))
 	val msb = MlGenByte.getMsb b
 in
 	if (msb = true) then 
 		(* if msb in this byte is 1, varint contains the next byte *)
-		parseVarint_core next_buff (i+1) next_val
+		parseVarint_core next_buff (i+1) next_val (ParsedByteCount(s+1))
 	else
-		(* final value is tuple of value and remaining buffer *)
-		(next_val, next_buff) 
+		(* final value is tuple of value, remaining buffer and
+		number of bytes parsed *)
+		(next_val, ParseResult(next_buff, ParsedByteCount(s)))
 end
 
-fun parseVarint buff = parseVarint_core buff 0 0
+fun parseVarint buff = parseVarint_core buff 0 0 (ParsedByteCount(0))
 
 fun parseKey buff = 
 let
-	val (v, next_buff) = parseVarint buff
+	val (v, ParseResult(next_buff, parsedByteCount)) = parseVarint buff
 	(* Type of the key is represented by last 3 bits. *)
-	val code = IntInf.andb(v, 7)
+	val code_ = Code(IntInf.andb(v, 7))
 	(* Field number is represented by the remaining bits. *)
-	val tag = IntInf.~>>(v, Word.fromInt 3)
+	val tag_ = Tag(IntInf.~>>(v, Word.fromInt 3))
 in
-	((tag, code), next_buff)
+	((tag_, code_), ParseResult(next_buff, parsedByteCount))
 end
 
-fun parseFixed i remaining buff prev_val = 
+fun parseFixed totalBytes i remaining buff prev_val = 
 let
 	val (b, next_buff) = ByteBuffer.nextByte buff
 	val next_val = 
-		prev_val + IntInf.<<((MlGenByte.toInt (MlGenByte.getTail b)), Word.fromInt(i * 7))
+		prev_val + IntInf.<<((MlGenByte.toInt (MlGenByte.getTail b)), 
+			Word.fromInt(i * 7))
 in
 	if (i > 1) then
-		parseFixed (i+1) (remaining-1) next_buff next_val
+		parseFixed totalBytes (i+1) (remaining-1) next_buff next_val
 	else
-		(next_val, next_buff)
+		(next_val, ParseResult(next_buff, ParsedByteCount(totalBytes)))
 end
 
-fun parse32 buff = parseFixed 0 4 buff 0
+fun parse32 buff = parseFixed 4 0 4 buff 0
 
-fun parse64 buff = parseFixed 0 8 buff 0
+fun parse64 buff = parseFixed 8 0 8 buff 0
 
-(* Returns message body and remaining buffer. *)
+(* Tested against encodeString *)
+fun parseString buff = 
+let 
+	(*
+	val ((tag_, code_), ParseResult(buff, ParsedByteCount(keyBytes))) = 
+		parseKey buff
+	*)
+	val (length, ParseResult(buff, ParsedByteCount(lenBytes))) =
+		parseVarint buff
+	val (body, buff) = ByteBuffer.nextFixedBlock buff length
+	val string_value = Byte.bytesToString body
+in
+	(* should return tag too? *)
+	(string_value, ParseResult(buff, ParsedByteCount(keyBytes + lenBytes + length)))
+end
+
+(*
+fun parseInt32 buff = 
+let
+	val ((tag_, code_), ParseResult(buff, ParsedByteCount(keyBytes))) = 
+		parseKey buff
+	val (v, ParseResult(next_buff, ParsedByteCount(valueBytes))) = 
+		parseVarint buff
+in	
+	if (code_ <> Code(0)) then
+		raise Exception(PARSE, "Attempting to parse wrong wire type.")
+	else
+		(* Return (tag, enum value) pair together with remaining buffer *)
+		((v, tag_), 
+			ParseResult(buff, ParsedByteCount(keyBytes + valueBytes)))
+end*)
+
+fun parseInt32 buff = parseVarint buff
+fun parseInt64 buff = parseVarint buff
+
+(*
+fun parse_message buff fields_function = 
+let
+	val (length, ParseResult(buff, ParsedByteCount(lenBytes))) =
+		parseVarint buff
+	val 
+in
+end
+*)
+
+(*
 fun parse_message_body buff = 
 let
 	val (length, buff) = parseVarint buff
@@ -115,32 +175,38 @@ in
 	(message, buff)
 end
 
+
 fun parse_message buff expected_tag = 
 let
-	val ((tag, code), buff) = parseKey buff
+	val ((tag_, code_), buff) = parseKey buff
 	val (length, buff) = parseVarint buff
 in
-	if (code <> 2) then
+	if (code_ <> Code(2)) then
 		raise Exception(PARSE, "Attempting to parse wrong wire type.")
-	else if (tag <> expected_tag) then
+	else if (tag_ <> expected_tag) then
 		raise Exception(PARSE, "Parsed tag does not match expected tag.")
 	else
 		(* Returns resulting message, as well as remaining buffer. *)
 		parse_message_body buff
 end
+*)
+
+fun parseEnum buff = parseVarint buff
 
 (*
-fun parseNext buff = 
+fun parseEnum buff = 
 let
-	val ((tag, code), next_buff) = parseKey buff
+	val ((tag_, code_), ParseResult(buff, ParsedByteCount(keyBytes))) = 
+		parseKey buff
+	val (enum_val, ParseResult(buff, ParsedByteCount(valueBytes))) = 
+		parseVarint buff 
 in
-	case code of 0 => parseVarint next_buff
-			   | 1 => parse64 next_buff
-			   | 2 =>
-			   | 3 => raise Exception("Groups are unsupported.")
-			   | 4 => raise Exception("Groups are unsupported.")
-			   | 5 => parse32 next_buff
-			   | n => raise Exception("Wire type unsupported.")
+	if (code_ <> Code(0)) then
+		raise Exception(PARSE, "Attempting to parse wrong wire type.")
+	else
+		(* Return (tag, enum value) pair together with remaining buffer*)
+		((enum_val, tag_), 
+			ParseResult(buff, ParsedByteCount(keyBytes + valueBytes)))
 end
 *)
 
@@ -193,23 +259,22 @@ in
 end
 
 (* Encodes the (tag, code) pair into a varint representing the key. *)
-fun encodeKey (tag, code) = 
-let 
-	val key = IntInf.orb(IntInf.<<(tag, Word.fromInt 3), code)
+fun encodeKey (Tag(t), Code(c)) = 
+let
+	val key = IntInf.orb(IntInf.<<(t, Word.fromInt 3), c)
 in
-	if (code < 0 orelse code > 5) then
+	if (c < 0 orelse c > 5) then
 		raise Exception(ENCODE, "Attemping to encode unsupported wire type")
 	else 
 		encodeVarint key
-end		
-
+end
 
 (* Encodes a string with a specific tag by prefixing it with key 
 and length. *)
-fun encodeString s tag = 
+fun encodeString (s, tag_) = 
 let
 	(* Wire type of string is 2 (length delimited). *)
-	val encoded_key = encodeKey (tag, 2)
+	val encoded_key = encodeKey (tag_, Code(2))
 	val encoded_length = encodeVarint (String.size s)
 	val encoded_body = Word8Vector.tabulate ((String.size s),
 		fn i => Byte.charToByte (String.sub (s, i))
@@ -220,12 +285,50 @@ end
 
 (* Encodes a byte array with a specific tag by prefixing it with
 key and length. *)
-fun encodeByteArray bytes tag = 
+fun encodeByteArray (bytes, tag_) = 
 let
 	(* Wire type of byte sequence is 2 (length delimited). *)
-	val encoded_key = encodeKey (tag, 2)
+	val encoded_key = encodeKey (tag_, Code(2))
 	val encoded_length = encodeVarint (Word8Vector.length bytes)
 in
 	Word8Vector.concat [encoded_key, encoded_length, bytes]
 end
+
+(* Not really tested *)
+fun encodeRepeated f ([], tag_:tag) = (Word8Vector.fromList [])
+  | encodeRepeated f (x::xs, tag_:tag) = Word8Vector.concat 
+  	[(f (x, tag_)), (encodeRepeated f (xs, tag_))]
+
+fun encodeOptional f (NONE, tag_:tag) = (Word8Vector.fromList [])
+  | encodeOptional f (SOME(x), tag_:tag) = (f (x, tag_))
+
+(* Not really tested *)
+fun encodeMessage (body, tag_) = 
+let 
+	(* Code is 2, since type is message (fixed length) *)
+	val encoded_key = encodeKey (tag_, Code(2))
+	val length = Word8Vector.length body
+	val encoded_length = encodeVarint length
+in
+	Word8Vector.concat [encoded_key, encoded_length, body]
+end
+
+fun encodeInt32 (n, tag_) = 
+let 
+	val key = encodeKey (tag_, Code(0))
+	val v = encodeVarint n
+in
+	Word8Vector.concat [key, v]
+end
+
+
+fun encodeInt64 (n, tag_) = 
+let 
+	val key = encodeKey (tag_, Code(0))
+	val v = encodeVarint n
+in
+	Word8Vector.concat [key, v]
+end
+
+(* TODO add fixed32, 64 etc *)
 
--- examples/addressbook.pb.ml
+++ examples/addressbook.pb.ml
@@ -3,6 +3,8 @@ use "MlGenLib.ml";
 signature PHONETYPE =
 sig
   type t
+  val encode : t * tag -> Word8Vector.vector
+  val parse : ByteBuffer.buffer -> (t * tag) * parseResult
 end
 structure PhoneType :> PHONETYPE = 
 struct
@@ -10,6 +12,27 @@ struct
     | HOME
     | WORK
     | UNKNOWN
+  fun encode (e, tag) =
+    let
+      val v = case (e) of MOBILE => encodeVarint 0
+      | HOME => encodeVarint 1
+      | WORK => encodeVarint 2
+      | UNKNOWN => encodeVarint 3
+      val key = encodeKey (tag, Code(0))
+    in
+      Word8Vector.concat [key, v]
+    end
+  fun parse buff =
+    let
+      val ((e, tag), buff) = parseEnum buff
+      val enum_val = case (e) of 0 => MOBILE
+      | 1 => HOME
+      | 2 => WORK
+      | 3 => UNKNOWN
+      | n => raise Exception(PARSE, "Attempting to parse enum of unknown tag value.")
+    in
+      ((enum_val, tag), buff)
+    end
 end
 type phoneType = PhoneType.t
 
@@ -17,15 +40,21 @@ signature PERSON =
 sig
   structure PhoneType : sig
     type t
+    val encode : t * tag -> Word8Vector.vector
+    val parse : ByteBuffer.buffer -> (t * tag) * parseResult
   end
   type phoneType
   structure PhoneNumber : sig
     type t
+    val encode : t * tag -> Word8Vector.vector
+    val parseNextField : ByteBuffer.buffer -> t -> int -> (t * tag) * parseResult
     val set_number: t * string option -> unit
     val set_type_: t * phoneType option -> unit
   end
   type phoneNumber
   type t
+  val encode : t * tag -> Word8Vector.vector
+  val parseNextField : ByteBuffer.buffer -> t -> int -> (t * tag) * parseResult
   val set_name: t * string option -> unit
   val set_id: t * int option -> unit
   val set_email: t * string option -> unit
@@ -40,6 +69,25 @@ struct
     datatype t = MOBILE
       | HOME
       | WORK
+    fun encode (e, tag) =
+      let
+        val v = case (e) of MOBILE => encodeVarint 0
+        | HOME => encodeVarint 1
+        | WORK => encodeVarint 2
+        val key = encodeKey (tag, Code(0))
+      in
+        Word8Vector.concat [key, v]
+      end
+    fun parse buff =
+      let
+        val ((e, tag), buff) = parseEnum buff
+        val enum_val = case (e) of 0 => MOBILE
+        | 1 => HOME
+        | 2 => WORK
+        | n => raise Exception(PARSE, "Attempting to parse enum of unknown tag value.")
+      in
+        ((enum_val, tag), buff)
+      end
   end
   type phoneType = PhoneType.t
   structure PhoneNumber = 
@@ -52,6 +100,47 @@ struct
     (#number msg) := value
     fun set_type_ (msg, value) = 
     (#type_ msg) := value
+    fun encode (m, tag) = 
+      let
+        val l = [(encodeOptional encodeString) (!(#number m), Tag(1)), 
+        (encodeOptional PhoneType.encode) (!(#type_ m), Tag(2))]
+      in
+        Word8Vector.concat l
+      end
+    fun parseNextField buff obj remaining = 
+      if (remaining <= 0) then
+        ()
+      else
+        let
+          val ((Tag(t), Code(c)), parse_result) = parseKey buff
+          val ParseResult(buff, ParsedByteCount(keyByteCount)) = parse_result
+          val remaining = remaining - keyByteCount
+        in
+          if (remaining <= 0) then
+            raise Exception(PARSE, "Not enough bytes in message to parse the message fields.")
+          else case (t) of 1 => 
+            let
+              val (field_value, parse_result) = parseString buff
+              val ParseResult(buff, ParsedByteCount(parsed_bytes)) = parse_result
+            in
+              if (remaining > parsed_bytes) then
+                (set_number (obj, field_value);
+                parseNextField buff obj (remaining - parsed_bytes))
+              else
+                raise Exception(PARSE, "Error in matching the message length with fields length.")
+            end
+          | 2 => 
+            let
+              val (field_value, parse_result) = PhoneType.parse buff
+              val ParseResult(buff, ParsedByteCount(parsed_bytes)) = parse_result
+            in
+              if (remaining > parsed_bytes) then
+                (set_type_ (obj, field_value);
+                parseNextField buff obj (remaining - parsed_bytes))
+              else
+                raise Exception(PARSE, "Error in matching the message length with fields length.")
+            end
+      end
   end
   type phoneNumber = PhoneNumber.t
   type t = {
@@ -70,12 +159,79 @@ struct
   (#phones msg) := value
   fun add_phones (msg, value) = 
   (#phones msg) := value :: !(#phones msg)
+  fun encode (m, tag) = 
+    let
+      val l = [(encodeOptional encodeString) (!(#name m), Tag(1)), 
+      (encodeOptional encodeInt32) (!(#id m), Tag(2)), 
+      (encodeOptional encodeString) (!(#email m), Tag(3)), 
+      (encodeRepeated PhoneNumber.encode) (!(#phones m), Tag(4))]
+    in
+      Word8Vector.concat l
+    end
+  fun parseNextField buff obj remaining = 
+    if (remaining <= 0) then
+      ()
+    else
+      let
+        val ((Tag(t), Code(c)), parse_result) = parseKey buff
+        val ParseResult(buff, ParsedByteCount(keyByteCount)) = parse_result
+        val remaining = remaining - keyByteCount
+      in
+        if (remaining <= 0) then
+          raise Exception(PARSE, "Not enough bytes in message to parse the message fields.")
+        else case (t) of 1 => 
+          let
+            val (field_value, parse_result) = parseString buff
+            val ParseResult(buff, ParsedByteCount(parsed_bytes)) = parse_result
+          in
+            if (remaining > parsed_bytes) then
+              (set_name (obj, field_value);
+              parseNextField buff obj (remaining - parsed_bytes))
+            else
+              raise Exception(PARSE, "Error in matching the message length with fields length.")
+          end
+        | 2 => 
+          let
+            val (field_value, parse_result) = parseInt32 buff
+            val ParseResult(buff, ParsedByteCount(parsed_bytes)) = parse_result
+          in
+            if (remaining > parsed_bytes) then
+              (set_id (obj, field_value);
+              parseNextField buff obj (remaining - parsed_bytes))
+            else
+              raise Exception(PARSE, "Error in matching the message length with fields length.")
+          end
+        | 3 => 
+          let
+            val (field_value, parse_result) = parseString buff
+            val ParseResult(buff, ParsedByteCount(parsed_bytes)) = parse_result
+          in
+            if (remaining > parsed_bytes) then
+              (set_email (obj, field_value);
+              parseNextField buff obj (remaining - parsed_bytes))
+            else
+              raise Exception(PARSE, "Error in matching the message length with fields length.")
+          end
+        | 4 => 
+          let
+            val (field_value, parse_result) = PhoneNumber.parse buff
+            val ParseResult(buff, ParsedByteCount(parsed_bytes)) = parse_result
+          in
+            if (remaining > parsed_bytes) then
+              (add_phones (obj, field_value);
+              parseNextField buff obj (remaining - parsed_bytes))
+            else
+              raise Exception(PARSE, "Error in matching the message length with fields length.")
+          end
+    end
 end
 type person = Person.t
 
 signature ADDRESSBOOK =
 sig
   type t
+  val encode : t * tag -> Word8Vector.vector
+  val parse : ByteBuffer.buffer -> (t * tag) * parseResult
   val set_people: t * person list -> unit
   val add_people: t * person -> unit
 end
@@ -89,6 +245,35 @@ struct
   (#people msg) := value
   fun add_people (msg, value) = 
   (#people msg) := value :: !(#people msg)
+  fun encode (m, tag) = 
+    let
+      val l = [(encodeRepeated Person.encode) (!(#people m), Tag(1))]
+    in
+      Word8Vector.concat l
+    end
+  fun parseNextField buff obj remaining = 
+    if (remaining <= 0) then
+      ()
+    else
+      let
+        val ((Tag(t), Code(c)), parse_result) = parseKey buff
+        val ParseResult(buff, ParsedByteCount(keyByteCount)) = parse_result
+        val remaining = remaining - keyByteCount
+      in
+        if (remaining <= 0) then
+          raise Exception(PARSE, "Not enough bytes in message to parse the message fields.")
+        else case (t) of 1 => 
+          let
+            val (field_value, parse_result) = Person.parse buff
+            val ParseResult(buff, ParsedByteCount(parsed_bytes)) = parse_result
+          in
+            if (remaining > parsed_bytes) then
+              (add_people (obj, field_value);
+              parseNextField buff obj (remaining - parsed_bytes))
+            else
+              raise Exception(PARSE, "Error in matching the message length with fields length.")
+          end
+    end
 end
 type addressBook = AddressBook.t
 
